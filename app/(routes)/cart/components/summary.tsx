1496bbe10f5160cbd66eb9f4cd1e8f57b87af96b
Abdul Hai Rawan <AbdulhaiRawan@gmail.com>
2024-12-04 11:14:27 +0430

    Update summary.tsx
    Fixed Next.js 14.1 prerendering Error

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
@@ -40,22 +40,26 @@ const Summary = () => {
   };
 
   return (
-    <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-      <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
-      <div className="mt-6 space-y-4">
-        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-medium text-gray-900">Order Total</div>
-          <Currency value={totalPrice} />
+    <Suspense fallback={<>Loading...</>}>
+      <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+        <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+        <div className="mt-6 space-y-4">
+          <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+            <div className="text-base font-medium text-gray-900">
+              Order Total
+            </div>
+            <Currency value={totalPrice} />
+          </div>
         </div>
+        <Button
+          disabled={items.length === 0}
+          onClick={onCheckout}
+          className="w-full mt-6"
+        >
+          Checkout
+        </Button>
       </div>
-      <Button
-        disabled={items.length === 0}
-        onClick={onCheckout}
-        className="w-full mt-6"
-      >
-        Checkout
-      </Button>
-    </div>
+    </Suspense>
   );
 };
 
