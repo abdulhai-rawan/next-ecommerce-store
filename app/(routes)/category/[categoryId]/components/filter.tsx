497faaa2717f3390fc619a0b91229856d200993a
Abdul Hai Rawan <AbdulhaiRawan@gmail.com>
2024-12-04 11:14:50 +0430

    Update filter.tsx
    Fixed Next.js 14.1 prerendering Error

--- app/(routes)/category/[categoryId]/components/filter.tsx
+++ app/(routes)/category/[categoryId]/components/filter.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { Color, Size } from "@/types";
+import React, { Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Button from "@/components/ui/button";
-import React from "react";
 import { cn } from "@/lib/utils";
 import qs from "query-string";
 
@@ -41,25 +41,27 @@ const Filter: React.FC<FilterProps> = ({ data, name, valueKey }) => {
   };
 
   return (
-    <div className="mb-8">
-      <h3 className="text-lg font-semibold">{name}</h3>
-      <hr className="my-4" />
-      <div className="flex flex-wrap gap-2">
-        {data.map((filter) => (
-          <div key={filter.id} className="flex items-center">
-            <Button
-              className={cn(
-                "rounded-md text-sm text-gray-800 p-2 bg-white border border-gray-300",
-                selectedValue === filter.id && "bg-black text-white"
-              )}
-              onClick={() => onClick(filter.id)}
-            >
-              {filter.name}
-            </Button>
-          </div>
-        ))}
+    <Suspense fallback={<>Loading...</>}>
+      <div className="mb-8">
+        <h3 className="text-lg font-semibold">{name}</h3>
+        <hr className="my-4" />
+        <div className="flex flex-wrap gap-2">
+          {data.map((filter) => (
+            <div key={filter.id} className="flex items-center">
+              <Button
+                className={cn(
+                  "rounded-md text-sm text-gray-800 p-2 bg-white border border-gray-300",
+                  selectedValue === filter.id && "bg-black text-white"
+                )}
+                onClick={() => onClick(filter.id)}
+              >
+                {filter.name}
+              </Button>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
